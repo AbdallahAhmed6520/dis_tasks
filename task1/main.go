f82ce86eb68cf26a6a724c3abbc954c1b1b2543c
Abdallah Ahmed <58490181+abdosedroho@users.noreply.github.com>
2022-03-30 21:59:21 +0200

    first commit

---
+++ task1/main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type Employee struct {
+	ID   int
+	Name string
+	City string
+}
+
+var db *sql.DB
+var err error
+
+func dbConn() {
+	dbDriver := "mysql"
+	dbUser := "root"
+	dbPass := ""
+	dbName := "testdb"
+
+	db, err = sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getAllEmployees() []Employee {
+	row, err := db.Query("SELECT * FROM employee")
+	if err != nil {
+		log.Fatal(err)
+	}
+	emp := Employee{}
+	employees := []Employee{}
+	for row.Next() {
+		err := row.Scan(&emp.ID, &emp.Name, &emp.City)
+		if err != nil {
+			log.Fatal(err)
+		}
+		employees = append(employees, emp)
+	}
+	return employees
+}
+
+func insert(name string, city string) {
+	stmt, err := db.Prepare("INSERT INTO employee(Name,City) VALUES (?	,?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r, err := stmt.Exec(name, city)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	affectedRows, err := r.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("The statement affected %d rows\n", affectedRows)
+
+}
+
+func update(id int, name string, city string) {
+	stmt, err := db.Prepare("UPDATE employee SET Name=?, City=? WHERE id=?")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r, err := stmt.Exec(name, city, id)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	affectedRows, err := r.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("The statement affected %d rows\n", affectedRows)
+}
+
+func delete(id int) {
+	stmt, err := db.Prepare("DELETE FROM employee WHERE id=?")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r, err := stmt.Exec(id)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	affectedRows, err := r.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("The statement affected %d rows\n", affectedRows)
+}
+
+func main() {
+	dbConn()
+	fmt.Println(getAllEmployees())
+	insert("abdallah", "assuit")
+	fmt.Println(getAllEmployees())
+
+	// update(1, "", "")
+	// delete(1)
+}
